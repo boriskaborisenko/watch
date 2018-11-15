0a5d7e68d27b5174ce5aa7f15578da2bbfbe6ad5
myNEWmac <boriskaborisenko@gmail.com>
2018-11-15 02:15:44 +0200

    createdBuild

--- src/scripts/watch.js
+++ src/scripts/watch.js
@@ -1,4 +1,4 @@
-function setClock(r){
+function setClock(){
     const seconds = document.querySelector(".seconds");
     const minutes = document.querySelector(".minutes");
     const hours = document.querySelector(".hours");
@@ -9,25 +9,19 @@ function setClock(r){
     const h = now.getHours();
     console.log(h, m, s);
 
-    var sd = (s + now.getMilliseconds() / 1000) / 60 * 360;
-    var md = m / 60 * 360;
-    var hd = (h + m / 60) / 12 * 360;
+    const sd = (s + now.getMilliseconds() / 1000) / 60 * 360;
+    const md = m / 60 * 360;
+    const hd = (h + m / 60) / 12 * 360;
 
     seconds.style.transform = 'rotate(' + sd + 'deg)';
     minutes.style.transform = 'rotate(' + md + 'deg)';
     hours.style.transform = 'rotate(' + hd + 'deg)';
     
-    let dir;
-    r ? dir="rev" : dir="spin";
-    
-      seconds.classList.add(dir);
-      minutes.classList.add(dir);
-      hours.classList.add(dir);
-      
-    
 }
 
-const reverse = false;
+
 document.addEventListener('DOMContentLoaded', function () {
-    setClock(reverse);
+    setInterval(()=>{
+        setClock();
+    },1000);
 });
